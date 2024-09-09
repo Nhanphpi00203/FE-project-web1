6b7d0cebd24719b56ef572025916d0e69da15587
nghia00200 <nghialmpi00200@gmail.com>
2024-09-09 11:07:57 +0700

    feat: add Route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "../components/ErrorBoundary.jsx";
+import LoadingPage from "../components/loading/LoadingPages.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Suspense fallback={<LoadingPage loading={true} />}>
+            <App />
+          </Suspense>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
