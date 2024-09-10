3d704df2bef503adb5a464d10e5cf9d42a98b99d
nghia00200 <nghialmpi00200@gmail.com>
2024-09-10 10:13:38 +0700

    feat: responsive + login

--- src/pages/User/MyAccount.jsx
+++ src/pages/User/MyAccount.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react"
 
 const MyAccount = () => {
+    const [isregister, setIsregister] = useState(true)
     return (
         <div className="container mx-auto flex justify-center items-center py-[100px]">
             <div className="w-[500px]  h-[500px]">
                 <h1 className="text-[35px] text-center ">My account</h1>
                 <div className=" text-center h-[50px] my-[50px] w-[500px] bg-[#EFEFEF] rounded flex justify-between items-center pl-1">
-                    <div className="bg-[white] w-[250px] h-[40px] rounded text-[18px] flex items-center justify-center shadow-md">
-                        Sign in
+
+                    {isregister ? <div className="text-center h-[50px] my-[50px] w-[500px] bg-[#EFEFEF] rounded flex justify-between items-center pl-1 mr-[10px]">
+                        <div className="ease-in-out duration-300 bg-[white] w-[240px] h-[40px] rounded text-[18px] flex items-center justify-center shadow-md">
+                            <p > Sign in</p>
+                        </div>
+                        <div  onClick={()=>setIsregister(false)} className="ease-in-out duration-300 cursor-pointer flex justify-center items-center w-[240px] h-[40px] text-[18px]">
+                            <p> Register</p>
+                        </div>
                     </div>
-                    <div className="flex justify-center items-center w-[250px] h-[40px] text-[18px]">
-                        Register
-                    </div>
+                        :
+                        <div className=" text-center h-[50px] my-[50px] w-[500px] bg-[#EFEFEF] rounded flex justify-between items-center pl-1">
+                            <div onClick={()=>setIsregister(true)} className="cursor-pointer w-[240px] h-[40px] rounded text-[18px] flex items-center justify-center ">
+                                <p > Sign in</p>
+                            </div>
+                            <div className="flex justify-center items-center w-[240px] h-[40px] text-[18px] shadow-md bg-[white] mr-[10px] rounded ">
+                                <p> Register</p>
+                            </div>
+                        </div>
+                    }
+
+
                 </div>
-                <form className="">
+                {isregister ?  <form className="">
                     <div> <input type="email" required placeholder="Email" className="border-solid border-b-2 border-[#D8D8D8] text-[17px] outline-none p-1 w-[500px] my-[10px]" /></div>
                     <div ><input type="password" required placeholder="Password" className="border-solid border-b-2 border-[#D8D8D8] text-[17px] outline-none p-1 w-[500px] my-[10px]" /></div>
                     <div className="flex ">
@@ -24,6 +41,21 @@ const MyAccount = () => {
                     </button>
                     <a className="flex justify-center text-[15px] mt-[10px]" href="#">Have you forgotten your password?</a>
                 </form>
+                :
+                <form className="">
+                      <div> <input type="name" required placeholder="Name" className="border-solid border-b-2 border-[#D8D8D8] text-[17px] outline-none p-1 w-[500px] my-[10px]" /></div>
+                <div> <input type="email" required placeholder="Email" className="border-solid border-b-2 border-[#D8D8D8] text-[17px] outline-none p-1 w-[500px] my-[10px]" /></div>
+                <div ><input type="password" required placeholder="Password" className="border-solid border-b-2 border-[#D8D8D8] text-[17px] outline-none p-1 w-[500px] my-[10px]" /></div>
+                
+                <button type="submit" className=" rounded flex justify-center items-center w-[500px] h-[50px] mt-[50px] bg-black text-white text-[17px]" >
+                    REGISTER
+                </button>
+                <a className="flex justify-center text-[15px] mt-[10px]" href="#">
+                    {isregister ? ' Have you forgotten your password?': <p onClick={()=>setIsregister(true)}>Already have an account?</p>}
+                   </a>
+            </form>
+            }
+               
             </div>
         </div>
     )
