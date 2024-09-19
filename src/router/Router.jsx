f24223bbc77e9cad1b78076de67c429764b7f08d
nghia00200 <nghialmpi00200@gmail.com>
2024-09-19 21:12:40 +0700

    feat: add cart

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -13,6 +13,11 @@ const Account =  lazy(() => import('../pages/Account/Account'))
 const Shop = lazy(() => import('../pages/Shop/Shop'))
 const Product = lazy(() => import('../pages/Shop/Product'))
 //
+
+//order
+const ViewOrder  = lazy(() => import('../pages/Order/ViewOrder'))
+const Cart = lazy(() => import('../pages/Order/Cart'))
+//
 const Pages404 = lazy(() => import('../pages/404/Pages404'))
 const Blog = lazy(() => import('../pages/Blog/Blog'))
 const SingleBlog = lazy(() => import('../pages/Blog/Blog-Single-NoSidebar'))
@@ -36,6 +41,11 @@ export default function AppRoutes() {
         {/* Shop */}
         <Route path="/Shop" element={<Shop />} />
         <Route path="/Product" element={<Product />} />
+        {/*------*/}
+
+          {/* Order */}
+          <Route path="/View-order" element={<ViewOrder />} />
+          <Route path="/Cart" element={<Cart />} />
         {/*------*/}
       </Routes>
       <Footer />
